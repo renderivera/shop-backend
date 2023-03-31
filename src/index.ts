4b0291a7fad1c4d7846ee1f4511e1a5a095e7542
raunchard <reinhard.zach@gmail.com>
2023-03-31 12:10:41 +0300

    log cors

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ require("dotenv").config();
 const prisma = new PrismaClient();
 const app = express();
 const port = 3000;
+
+console.log(process.env.CLIENT_URL);
+
 app.use(cors({ origin: process.env.CLIENT_URL }), express.json());
 
 const stripe = new Stripe(process.env.STRIPE_SK as string, {
